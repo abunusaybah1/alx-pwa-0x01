61cc1d6675faccc4922a952f06bb3fa1745dbff0
abunusaybah <your_github_email@example.com>
2025-12-16 11:52:43 +0100

    troubleshort

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -4,7 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   reactStrictMode: true,
-  output: "export"
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
